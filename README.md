baddc2ee3646d8610971e04c48f8f9e2456141c1
Parsa Hejabi <parsa.hejabi@gmail.com>
2024-04-01 16:56:17 -0700

    Some changes :))

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-## Data  
+# Data  
+
 Balderdash words set1 from [wordnik](https://www.wordnik.com/lists/balderdash-game-words): [link](https://drive.google.com/file/d/18ANahetu6RynqINS1Ug5Fzmn87jqjXpQ/view?usp=share_link)  
 Balderdash words set2 from [justenglish](https://justenglish.me/2015/03/10/the-100-funniest-words-in-english/): [link](https://drive.google.com/file/d/1-7furyX1JQfdcnBlTHAqCMlgD9DiM7PK/view?usp=share_link)  
 Preliminary results on set1: [link](https://drive.google.com/file/d/1KtCVf34kTgy_lSCK5C9wpNSMOvYO-zhO/view?usp=share_link)  
--- base_data_gen.py
+++ base_data_gen.py
@@ -10,16 +10,18 @@ import pandas as pd
 from openai import OpenAI
 from evaluate import load
 
+from dotenv import load_dotenv
 
-client = OpenAI(
-  api_key='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'
-)
+load_dotenv()
+
+
+client = OpenAI(api_key="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")
 
 bertscore = load("bertscore")
 
-df = pd.read_csv('./Words2.csv')
-df = df.iloc[:50,:]
-df.reset_index(inplace=True, drop = True)
+df = pd.read_csv("./Words2.csv")
+df = df.iloc[:50, :]
+df.reset_index(inplace=True, drop=True)
 
 
 defenition = []
@@ -27,42 +29,43 @@ misinformation = []
 # defscore = []
 # misinfoscore = []
 for i in range(len(df)):
-    if i%10 == 0:
-        print(i//10)
+    if i % 10 == 0:
+        print(i // 10)
         db = pd.DataFrame()
-        db['definition'] = defenition
-        db['misinformation'] = misinformation
-        db.to_csv('./Words2_GPT35.csv', index = False)
+        db["definition"] = defenition
+        db["misinformation"] = misinformation
+        db.to_csv("./Words2_GPT35.csv", index=False)
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "user",
-                "content": "define the word "+ df['words'][i] +" in one sentence.",
+                "content": "define the word " + df["words"][i] + " in one sentence.",
             }
         ],
         model="gpt-3.5-turbo",
     )
     defenition.append(chat_completion.choices[0].message.content)
-    # defscore.append(bertscore.compute(predictions=[chat_completion.choices[0].message.content], 
+    # defscore.append(bertscore.compute(predictions=[chat_completion.choices[0].message.content],
     #                                   references=[df['def'][i]], lang="en"))
-    
+
     chat_completion = client.chat.completions.create(
         messages=[
             {
                 "role": "user",
-                "content": "Suppose you are playing Balderdash and you want to deceive your opponants with a deceiving definition. define "+ df['words'][i] +" in one sentence.",
+                "content": "Suppose you are playing Balderdash and you want to deceive your opponants with a deceiving definition. define "
+                + df["words"][i]
+                + " in one sentence.",
             }
         ],
         model="gpt-3.5-turbo",
     )
     misinformation.append(chat_completion.choices[0].message.content)
-    # misinfoscore.append(bertscore.compute(predictions=[chat_completion.choices[0].message.content], 
+    # misinfoscore.append(bertscore.compute(predictions=[chat_completion.choices[0].message.content],
     #                                   references=[df['def'][i]], lang="en"))
-    
-df['definition'] = defenition
-df['misinformation'] = misinformation
+
+df["definition"] = defenition
+df["misinformation"] = misinformation
 # df['defscore'] = defscore
 # df['misinfoscore'] = misinfoscore
 
-df.to_csv('./Words2_GPT35.csv', index = False)
-
+df.to_csv("./Words2_GPT35.csv", index=False)
